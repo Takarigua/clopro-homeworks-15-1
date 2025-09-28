515a077acf7237dd228730b4a8e1343148739dc1
Takarigua <49216259+Takarigua@users.noreply.github.com>
2025-09-28 17:17:26 +1100

    Create instances.tf

---
+++ instances.tf
@@ -0,0 +1,81 @@
+# NAT-инстанс
+resource "yandex_compute_instance" "nat" {
+  name        = "nat-instance"
+  platform_id = "standard-v1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd80mrhj8fl2oe87o4e1" # NAT-образ
+      size     = 8                       # минимальный допустимый размер диска
+    }
+  }
+
+  network_interface {
+    subnet_id  = yandex_vpc_subnet.public.id
+    ip_address = "192.168.10.254"
+    nat        = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+# Публичная виртуалка
+resource "yandex_compute_instance" "public_vm" {
+  name        = "public-vm"
+  platform_id = "standard-v1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd887k3tohr44p393noh" # Ubuntu 22.04
+      size     = 8
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.public.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+# Приватная виртуалка
+resource "yandex_compute_instance" "private_vm" {
+  name        = "private-vm"
+  platform_id = "standard-v1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd887k3tohr44p393noh" # Ubuntu 22.04
+      size     = 8
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.private.id
+    nat       = false
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
